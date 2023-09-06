95afd3a94eb071de0a87268de081f863af1e5f29
Blag <atejada@gmail.com>
2023-09-06 10:32:33 -0400

    Update Webhooks.rb

--- Webhooks.rb
+++ Webhooks.rb
@@ -3,6 +3,13 @@ require 'nylas'
 require 'sinatra'
 require "sinatra/config_file"
 
+port = ENV["PORT"] || "4567"
+configure { 
+  set :server, :puma
+  set :bind, '0.0.0.0'
+  set :port, port
+}
+
 # We display the database content
 get '/' do
     if params.include? "challenge"
