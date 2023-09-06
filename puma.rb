5db8ae4216aed40556d8329611727d573cd8dc87
Blag <atejada@gmail.com>
2023-09-06 09:46:16 -0400

    Create puma.rb

---
+++ puma.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env puma
+# frozen_string_literal: true
+
+threads_count = ENV.fetch('PUMA_THREADS') { 5 }.to_i
+threads threads_count, threads_count
+
+port ENV.fetch('PORT') { 4567 }
+
+workers ENV.fetch('WORKERS') { 1 }.to_i
