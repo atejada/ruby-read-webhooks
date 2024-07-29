418b71dea8a875b0779de4128f4ed29ca01125d6
Blag <atejada@gmail.com>
2024-07-29 05:18:10 -0500

    Update app.rb
    Testing Bounce Detection

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require 'nylas'
 require 'sinatra'
 require "sinatra/config_file"
 
-webhook = Data.define(:id, :date, :title, :description, :participants, :status)
+webhook = Data.define(:id, :time, :bounce_reason, :bounce_address)
+#webhook = Data.define(:id, :date, :title, :description, :participants, :status)
 webhooks = Array.new
 
 get '/webhook' do
@@ -26,36 +27,44 @@ post '/webhook' do
     )
 	
 # Query parameters
-query_params = {
-    calendar_id: ENV["CALENDAR_ID"]
-}	
+#query_params = {
+#    calendar_id: ENV["CALENDAR_ID"]
+#}	
 	
 # We read the webhook information and store it on the data class
     request.body.rewind
     model = JSON.parse(request.body.read)
-    if model["data"]["object"]["calendar_id"] == ENV["CALENDAR_ID"]
-        event, _request_id = nylas.events.find(identifier: ENV["GRANT_ID"], object_id: model["data"]["object"]["id"], query_params: query_params) 
-        participants = ""
-        event_date = ""
-        event[:participants].each do |elem|
-            participants += "#{elem[:email]}; "
-        end
     
-        case event[:when][:object]
-            when 'timespan'
-	        start_time = Time.at(event[:when][:start_time]).strftime('%m-%d-%Y %H:%M:%S')
-	        end_time = Time.at(event[:when][:end_time]).strftime('%m-%d-%Y %H:%M:%S')
-	        event_date = "#{start_time} to #{end_time}"
-	    when 'datespan'
-                start_time = event[:when][:start_date].strftime('%m-%d-%Y')
-	        end_time = event[:when][:end_date].strftime('%m-%d-%Y')
-	        event_date = "#{start_time} to: #{end_time}"	    
-	    when 'date'
-	        start_time = event[:when][:date].strftime('%m-%d-%Y')
-	        event_date = "#{start_time} "
-        end
-        hook = webhook.new(event[:id], event_date, event[:title], event[:description], participants, event[:status])
-        webhooks.append(hook)
+    # Bounce detection starts
+    model["data"]["object"]
+
+    hook = webhook.new(model["data"]["id"], model["data"]["time"], model["data"]["object"]["bounce_reason"], model["data"]["object"]["bounce_address"])
+    webhooks.append(hook)
+    # Bounce detection ends
+
+    #if model["data"]["object"]["calendar_id"] == ENV["CALENDAR_ID"]
+    #    event, _request_id = nylas.events.find(identifier: ENV["GRANT_ID"], object_id: model["data"]["object"]["id"], query_params: query_params) 
+    #    participants = ""
+    #    event_date = ""
+    #    event[:participants].each do |elem|
+    #        participants += "#{elem[:email]}; "
+    #    end
+    
+    #    case event[:when][:object]
+    #        when 'timespan'
+    #        start_time = Time.at(event[:when][:start_time]).strftime('%m-%d-%Y %H:%M:%S')
+    #        end_time = Time.at(event[:when][:end_time]).strftime('%m-%d-%Y %H:%M:%S')
+    #        event_date = "#{start_time} to #{end_time}"
+    #      when 'datespan'
+    #          start_time = event[:when][:start_date].strftime('%m-%d-%Y')
+    #          end_time = event[:when][:end_date].strftime('%m-%d-%Y')
+    #          event_date = "#{start_time} to: #{end_time}"	    
+    #      when 'date'
+    #          start_time = event[:when][:date].strftime('%m-%d-%Y') 
+    #          event_date = "#{start_time} "
+    #end
+    #    hook = webhook.new(event[:id], event_date, event[:title], event[:description], participants, event[:status])
+    #    webhooks.append(hook)
     end
 	status 200
 	"Webhook received"	
